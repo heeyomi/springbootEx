e53ca1c1594a2f26f112d770eee25b3968c4ed85
heeyomi <heeyomi@heeyomi>
2021-06-23 13:54:06 +0900

    init commit

---
+++ src/test/java/springbootex/HelloWorldApplicationTest.java
@@ -0,0 +1,13 @@
+package springbootex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @ExtendWith(SpringExtension.class)
+// @ContextConfiguration(classes= {HelloWorldApplication.class})
+@SpringBootTest
+public class HelloWorldApplicationTest {
+	@Test
+	public void contextLoads() {
+	}
+}
